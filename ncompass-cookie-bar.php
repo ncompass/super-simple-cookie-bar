4d6cfb578d1936e59146be8cdf3a55db07621753
Jeremy Stone <jim@ncompass.co.uk>
2022-05-05 16:34:13 +0100

    Revert "removed tagged versioning"
    This reverts commit 8e9d2d4c24f47a4a40235095e9d8bf8627a22620.

--- ncompass-cookie-bar.php
+++ ncompass-cookie-bar.php
@@ -9,6 +9,7 @@
  * License:           GPLv2 or later
  * Text Domain:       ncompass-cookie-bar
  * GitHub Plugin URI: https://github.com/ncompass/ncompass-cookie-bar
+ * Release Asset:     true
  * Primary Branch:    main
  */
 
